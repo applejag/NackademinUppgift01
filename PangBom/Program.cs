3fb3862ae544fd1176bd46ffdb70716b1830fcd8
kalle (jag) <kalle.jillheden@me.com>
2017-08-29 19:45:10 +0200

    PangBom

--- PangBom/Program.cs
+++ PangBom/Program.cs
@@ -10,6 +10,31 @@ namespace PangBom
     {
         static void Main(string[] args)
         {
+            // 39st elever
+            for (int i = 1; i <= 39; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("PangBom");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pang");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Bom");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
